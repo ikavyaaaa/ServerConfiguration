359582a390c551694c26e43a1f4580548c633ecb
Kavya Krishna <kavyakrishna@zktecodev.com>
2024-11-13 14:09:47 +0530

    initial commit

---
+++ ServerConfiguration/ServerConfiguration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ServerConfiguration
+//
+//  Created by Kavya Krishna K. on 13/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
